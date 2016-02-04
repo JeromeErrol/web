f081ae63a1a74d2298b198d1d753845172951d61
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2016-02-04 15:47:10 +0100

    add spring-website-template project

---
+++ spring-website-template/src/main/java/com/template/SpringWebsiteTemplate.java
@@ -0,0 +1,12 @@
+package com.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsiteTemplate {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebsiteTemplate.class, args);
+    }
+}
